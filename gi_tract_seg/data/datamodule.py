e61348a331ed45efb7021382d77875039dea8c54
skolchenko <skolchenko@cellarity.com>
2022-05-01 20:59:19 +0000

    Resolving pre-commit-hook changes

--- gi_tract_seg/data/datamodule.py
+++ gi_tract_seg/data/datamodule.py
@@ -10,18 +10,19 @@ log = utils.get_logger(__name__)
 
 
 class GITractDataModule(LightningDataModule):
-    def __init__(self, 
-                data_path: str, 
-                images_path: str,
-                masks_path: str,
-                val_fold: int, 
-                test_fold: Optional[int] = None,
-                batch_size: int = 32,
-                num_workers: int = 8,
-                pin_memory: bool = True,
-                train_augmentations = A.Compose([A.Resize(256, 256)]),
-                val_augmentations = A.Compose([A.Resize(256, 256)]),
-                ):
+    def __init__(
+        self,
+        data_path: str,
+        images_path: str,
+        masks_path: str,
+        val_fold: int,
+        test_fold: Optional[int] = None,
+        batch_size: int = 32,
+        num_workers: int = 8,
+        pin_memory: bool = True,
+        train_augmentations=A.Compose([A.Resize(256, 256)]),
+        val_augmentations=A.Compose([A.Resize(256, 256)]),
+    ):
         super().__init__()
 
         self.save_hyperparameters(logger=False)
@@ -51,41 +52,33 @@ class GITractDataModule(LightningDataModule):
 
     def prepare_data(self):
         non_train_folds = [self.val_fold]
-        self.val_data =\
-            self.data.loc[self.data['fold'] == self.val_fold, :]
+        self.val_data = self.data.loc[self.data["fold"] == self.val_fold, :]
 
         if self.test_fold is not None:
-            self.test_data =\
-                self.data.loc[self.data['fold'] == self.test_fold, :]
+            self.test_data = self.data.loc[self.data["fold"] == self.test_fold, :]
             non_train_folds += [self.test_fold]
 
-        self.train_data =\
-            self.data.loc[~self.data['fold'].isin(non_train_folds), :]
+        self.train_data = self.data.loc[~self.data["fold"].isin(non_train_folds), :]
 
     def setup(self, stage: Optional[str] = None):
         log.info("Setting up data in datamodule")
-        if not self.train_dataset\
-                and not self.val_dataset\
-                and not self.test_dataset:
+        if not self.train_dataset and not self.val_dataset and not self.test_dataset:
             self.train_dataset = GITractDataset(
                 self.train_data,
                 self.images_path,
                 self.masks_path,
                 self.train_augmentations,
-                )
+            )
             self.val_dataset = GITractDataset(
-                self.val_data,
-                self.images_path,
-                self.masks_path,
-                self.val_augmentations
-                )
+                self.val_data, self.images_path, self.masks_path, self.val_augmentations
+            )
             if self.test_fold is not None:
                 self.test_dataset = GITractDataset(
                     self.test_data,
                     self.images_path,
                     self.masks_path,
-                    self.val_augmentations
-                    )
+                    self.val_augmentations,
+                )
 
     def train_dataloader(self):
         return DataLoader(
